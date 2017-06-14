36cbe4ac5236a4ecf6a5ea3de3fb2d7928e7a53b
Jeffrey Gillis <jeffrey.gillis1@gmail.com>
2017-06-13 20:04:29 -0400

    WIP: Start organizing by day

--- organize_recordings.rb
+++ organize_recordings.rb
@@ -4,33 +4,42 @@ require 'pry'
 require 'fileutils'
 
 OUTPUT_DIR = './content/recordings'
-START_YEAR = 2017
 
 files = Dir.glob('./recordings/*.yaml')
+month_grouped_files = files.group_by { |x| x.scan(/\d{4}-\d{2}/).first }
+months = month_grouped_files.keys.sort {|x,y| y <=> x }
 
-START_YEAR.upto(Time.now.year).each do |year|
-  last_month = Time.now.year == year ? Time.now.month : 12
-  1.upto(last_month) do |month|
+months.each do |month|
+  puts ""
+  puts "********** #{month} ***********"
+  files = month_grouped_files[month].sort {|x,y| y <=> x }
+  date_grouped_files = files.group_by { |x| x.scan(/\d{4}-\d{2}-\d{2}/).first }
 
-    formatted_month = format('%02d', month)
-    selected_files = files.select {|f| ! f.index("#{year}-#{formatted_month}").nil? }
-    unless selected_files.empty?
-      output_path = File.join(File.join(OUTPUT_DIR, year.to_s, formatted_month))
-
-      # Make sure directories exist
-      unless Dir.exists?(output_path)
-        FileUtils.mkdir_p(output_path)
-      end
-
-      # Copy files to directories
-      selected_files.each do |file|
-        puts "Copying file: #{file} to #{output_path}"
-        FileUtils.cp(file, output_path)
-      end
-
-      # Copy template index file for directory
-      puts "* Creating index file ./content/recordings/#{year}/#{formatted_month}.html"
-      FileUtils.cp('./recordings/index.html', "./content/recordings/#{year}/#{formatted_month}.html")
+  date_grouped_files.keys.each do |date|
+    date_grouped_files[date].each do |file|
+      destination = file.gsub(/(\d{4})-(\d{2})-(\d{2})-/,'\1/\2/\3/')
+      dir = File.dirname(destination)
+      FileUtils.mkdir_p(dir) unless Dir.exists?(dir)
+      puts destination
     end
   end
 end
+
+
+
+#    selected_files = files.select {|f| ! f.index("#{year}-#{formatted_month}").nil? }
+#    unless selected_files.empty?
+#
+#
+#      # Copy files to directories
+#      selected_files.each do |file|
+#        puts "Copying file: #{file} to #{output_path}"
+#        FileUtils.cp(file, output_path)
+#      end
+#
+#      # Copy template index file for directory
+#      puts "* Creating index file ./content/recordings/#{year}/#{formatted_month}.html"
+#      FileUtils.cp('./recordings/month.html', "./content/recordings/#{year}/#{formatted_month}.html")
+#    end
+#  end
+#end
