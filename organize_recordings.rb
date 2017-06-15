543e979ecdfc4b55c0720d0d98ac3f11dba47a11
Jeffrey Gillis <jeffrey.gillis1@gmail.com>
2017-06-14 20:02:08 -0400

    Update content generation script

--- organize_recordings.rb
+++ organize_recordings.rb
@@ -4,42 +4,34 @@ require 'pry'
 require 'fileutils'
 
 OUTPUT_DIR = './content/recordings'
+MONTH_TEMPLATE = './recordings/month.html'
+DAY_TEMPLATE = './recordings/day.html'
 
-files = Dir.glob('./recordings/*.yaml')
-month_grouped_files = files.group_by { |x| x.scan(/\d{4}-\d{2}/).first }
-months = month_grouped_files.keys.sort {|x,y| y <=> x }
-
-months.each do |month|
-  puts ""
-  puts "********** #{month} ***********"
-  files = month_grouped_files[month].sort {|x,y| y <=> x }
-  date_grouped_files = files.group_by { |x| x.scan(/\d{4}-\d{2}-\d{2}/).first }
-
-  date_grouped_files.keys.each do |date|
-    date_grouped_files[date].each do |file|
-      destination = file.gsub(/(\d{4})-(\d{2})-(\d{2})-/,'\1/\2/\3/')
-      dir = File.dirname(destination)
-      FileUtils.mkdir_p(dir) unless Dir.exists?(dir)
-      puts destination
-    end
+def create_output_destination(files)
+  files.each do |file|
+    destination = file.gsub(/(\d{4})-(\d{2})-(\d{2})-/,'\1/\2/\3/')
+    dir = File.dirname(destination)
+    FileUtils.mkdir_p(dir) unless Dir.exists?(dir)
+    output_path = File.join('./content', destination)
+    puts "#{file}: #{output_path}"
+    FileUtils.cp(file, output_path)
   end
 end
 
+def copy_templates(files, pattern, template)
+  pattern = Regexp.new(pattern)
+  puts pattern.inspect
+  paths = files.map {|x| x.scan(pattern).first.gsub("-", "/") }.uniq
+  full_paths = paths.map {|path| File.join(OUTPUT_DIR, path) }
 
+  full_paths.each do |path|
+    puts "#{template}: #{File.join(path, 'index.html')}"
+    FileUtils.cp(template, File.join(path, 'index.html'))
+  end
 
-#    selected_files = files.select {|f| ! f.index("#{year}-#{formatted_month}").nil? }
-#    unless selected_files.empty?
-#
-#
-#      # Copy files to directories
-#      selected_files.each do |file|
-#        puts "Copying file: #{file} to #{output_path}"
-#        FileUtils.cp(file, output_path)
-#      end
-#
-#      # Copy template index file for directory
-#      puts "* Creating index file ./content/recordings/#{year}/#{formatted_month}.html"
-#      FileUtils.cp('./recordings/month.html', "./content/recordings/#{year}/#{formatted_month}.html")
-#    end
-#  end
-#end
+end
+
+files = Dir.glob('./recordings/*.yaml').sort {|x, y| y <=> x }
+create_output_destination(files)
+copy_templates(files, '\d{4}-\d{2}', MONTH_TEMPLATE)
+copy_templates(files, '\d{4}-\d{2}-\d{2}', DAY_TEMPLATE)
